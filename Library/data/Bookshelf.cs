032f5dabed5a53fcbe07fb6b8e6a98a472cc34d2
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2023-03-31 22:23:27 +0200

    create Bookshelf class

---
+++ Library/data/Bookshelf.cs
@@ -0,0 +1,26 @@
+﻿using Library.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.data
+{
+    public class Bookshelf
+    {
+        private List<Book> _books;
+        public List<Book> Books { get { return _books; } }
+
+        public Bookshelf() 
+        {
+            _books = ListAllBooks();
+        }
+
+        private List<Book> ListAllBooks()
+        {
+            List<Book> list = new List<Book>();
+            return list;
+        }
+    }
+}
